ca6914cf12c4dac8d04fd0d225d94c8283178f0e
Gabriel Zavando <gabrielzavando@gmail.com>
2019-04-07 20:01:58 -0400

    Añadi una cuenta regresiva como elemento modular

--- src/js/globales/index.js
+++ src/js/globales/index.js
@@ -1,6 +1,8 @@
 import {toggleNav, animaciones} from '../dev/nav';
-import Slider from '../dev/slider'
+import Slider from '../dev/slider';
+import cuentaRegresiva from '../dev/cuenta-regresiva';
 
 toggleNav()
 animaciones()
 Slider()
+cuentaRegresiva()
