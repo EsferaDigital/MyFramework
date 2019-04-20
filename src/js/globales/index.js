b2756ba716530fd98d3d9a5e63b142701dd362aa
Gabriel Zavando <gabrielzavando@gmail.com>
2019-04-20 11:23:35 -0400

    añadi navegacion por tabs

--- src/js/globales/index.js
+++ src/js/globales/index.js
@@ -1,10 +1,12 @@
 import {toggleNav, animaciones} from '../dev/nav';
 // import SliderFade from '../dev/slider';
+import TabsNav from '../dev/tabs';
 import carruselNormal from '../dev/slider';
-import cuentaRegresiva from '../dev/cuenta-regresiva';
+// import cuentaRegresiva from '../dev/cuenta-regresiva';
 
 toggleNav()
 animaciones()
 carruselNormal()
+TabsNav()
 // SliderFade()
-cuentaRegresiva()
+// cuentaRegresiva()
